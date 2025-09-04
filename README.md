060684c078a9eb8cf1ab8d082bbbb96efd390395
LoganOReed <me@loganreed.org>
2025-09-03 23:25:33 -0400

    Ran A few Tests

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # Storm Drain
 TODO: Actually write readme :)
+
+Results from current setup with rainRate=0.375 and edges having 0.15 weight
+```
+Runoff: 82074.1550000871, Drainage: 78988.80000135278, NewFraction: 0.5095781025460098, PeakDischarge: 2.6100000000000003
+```
+
+rainRate=0.75
+```
+Runoff: 183895.65000011207, Drainage: 78988.80000135278, NewFraction: 0.6995303449827001, PeakDischarge: 4.260000000000001
+```
+
+rainRate=1.25
+```
+Runoff: 307315.6500000627, Drainage: 78988.80000135278, NewFraction: 0.7955270771510311, PeakDischarge: 6.260000000000001
+```
--- app/basicFlow.py
+++ app/basicFlow.py
@@ -21,8 +21,11 @@ class DrainGraph:
         self.fps = 10
         self.T = 3600
         self.drainedAmount = 0
+        self.runoffAmount = 0
         self.resolution = 10 # animation changes every "resolution" number of T steps
-        self.rainRate = 0.375 # (m^3/s) for each node, from calculations above
+        self.rainRate = 1.25 # (m^3/s) for each node, from calculations above
+        self.peakDischarge = 0.0 # peak discharge at a time step of runoff + drainage
+        self.newFraction = 0.0 # currently runoff/totalwater
         self.G = nx.DiGraph()
         self.G.add_nodes_from([i for i in range(0,17)])
         drainRates = [0.08 for i in range(1,17)]
@@ -33,41 +36,44 @@ class DrainGraph:
             self.G.nodes[i]["amount"] = 0.0
             self.G.nodes[i]["drainRate"] = drainRates[i-1]
         self.G.add_weighted_edges_from([ # 0 is runoff
-                (1, 2, 0.1),
-                (2, 3, 0.1),
-                (2, 6, 0.1),
-                (3, 7, 0.1),
-                (4, 3, 0.1),
-                (4, 8, 0.1),
-                (5, 1, 0.1),
-                (5, 6, 0.1),
-                (6, 10, 0.1),
-                (7, 6, 0.1),
-                (7, 8, 0.1),
-                (7, 11, 0.1),
-                (8, 12, 0.1),
-                (9, 5, 0.1),
-                (9, 10, 0.1),
-                (9, 13, 0.1),
-                (10, 14, 0.1),
-                (11, 15, 0.1),
-                (11, 10, 0.1),
-                (12, 11, 0.1),
-                (13, 14, 0.1),
+                (1, 2, 0.15),
+                (2, 3, 0.15),
+                (2, 6, 0.15),
+                (3, 7, 0.15),
+                (4, 3, 0.15),
+                (4, 8, 0.15),
+                (5, 1, 0.15),
+                (5, 6, 0.15),
+                (6, 10, 0.15),
+                (7, 6, 0.15),
+                (7, 8, 0.15),
+                (7, 11, 0.15),
+                (8, 12, 0.15),
+                (9, 5, 0.15),
+                (9, 10, 0.15),
+                (9, 13, 0.15),
+                (10, 14, 0.15),
+                (11, 15, 0.15),
+                (11, 10, 0.15),
+                (12, 11, 0.15),
+                (13, 14, 0.15),
                 (13, 0,  np.finfo(np.float32).max), #largest 32bit fl val
-                (14, 15, 0.1),
+                (14, 15, 0.15),
                 (14, 0,  np.finfo(np.float32).max), #largest 32bit fl val
                 (15, 0,  np.finfo(np.float32).max), #largest 32bit fl val
-                (16, 12, 0.1),
-                (16, 15, 0.1),
+                (16, 12, 0.15),
+                (16, 15, 0.15),
                 (16, 0,  np.finfo(np.float32).max) #largest 32bit fl val
                                         ])
         print(self.G.nodes.data())
 
     def update(self, frame):
         tCurr = frame*self.resolution
+
         # Actual update steps
         for i in range(self.resolution):
+            drainedAmt = 0.0
+            runoffAmt = 0.0
             for n in range(1,17):
         # 1. add water from rain 
                 self.G.nodes[n]['amount'] += self.rainRate
@@ -75,11 +81,13 @@ class DrainGraph:
                 dAmt = self.G.nodes[n]['drainRate']
                 if dAmt <= self.G.nodes[n]['amount']:
                     self.drainedAmount += dAmt
-                    self.G.nodes[n]['amount'] -= dAmt  # Example update logic
+                    drainedAmt += dAmt
+                    self.G.nodes[n]['amount'] -= dAmt
                 else:
                     self.drainedAmount += self.G.nodes[n]['amount']
+                    drainedAmt += self.G.nodes[n]['amount']
                     self.G.nodes[n]['amount'] = 0.0
-        # 3. update from edges edges
+        # 3. update from edges
                 available = self.G.nodes[n]['amount']
                 normal_edges = []
                 runoff_edges = []
@@ -107,9 +115,16 @@ class DrainGraph:
                     # Distribute up to capacity or what's available
                     sent = min(capacity, available)
                     self.G.nodes[v]['amount'] += sent
+                    self.runoffAmount += sent
+                    runoffAmt += sent
                     available -= sent  # update available for remaining edges
                 # After distributing, update the source node's amount
                 self.G.nodes[n]['amount'] = available
+
+            # update metadata info
+            if self.peakDischarge <= drainedAmt + runoffAmt:
+                self.peakDischarge = drainedAmt + runoffAmt
+                print(self.peakDischarge)
 
 
 
@@ -120,8 +135,8 @@ class DrainGraph:
             amounts = [self.G.nodes[n]['amount'] for n in self.G.nodes()]
             # Normalize for colormap
             # TODO: Update this when using an actual update function
-            # norm = plt.Normalize(min(amounts), max(amounts))
-            norm = plt.Normalize(0, 10000)
+            norm = plt.Normalize(min(amounts), max(amounts))
+            # norm = plt.Normalize(0, 10000)
             cmap = plt.cm.viridis
             return [cmap(norm(a)) for a in amounts]
 
@@ -151,6 +166,7 @@ class DrainGraph:
             indeg = self.G.in_degree(node)
             outdeg = self.G.out_degree(node)
             print(f"Node {node}: in={indeg}, out={outdeg}")
+        print(f"Runoff: {self.runoffAmount}, Drainage: {self.drainedAmount}, NewFraction: {self.runoffAmount / (self.runoffAmount + self.drainedAmount)}, PeakDischarge: {self.peakDischarge}")
 
 
 if __name__ == "__main__":
