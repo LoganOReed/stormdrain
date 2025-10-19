073337f32686d6d040041575b04f8cab48bf823e
LoganOReed <me@loganreed.org>
2025-10-18 20:26:04 -0400

    Fixed LargerExample Subcatchment Visualization code

--- app/subcatchmentExample.py
+++ app/subcatchmentExample.py
@@ -14,7 +14,7 @@ if __name__ == "__main__":
 
 
     # Create plot for disconnected subcatchments
-    g = SubcatchmentGraph(3)
+    g = SubcatchmentGraph()
     subcatchment = []
     for i in range(len(rainfall)):
         subcatchment.append(g.update(2*i,0.5,rainfall[i]))
@@ -27,17 +27,17 @@ if __name__ == "__main__":
     g.visualize(ts, subcatchment, "disconnected")
 
     # Create plot for subcatchments 0->1->2
-    rainfall = [0.0,0.5,1.0,0.75,0.5,0.25,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0]
-    rainfall = [e * 0.0254 for e in rainfall]
-    g = SubcatchmentGraph(3,2) # temp, 2 just indicates not None
-    subcatchment = []
-    for i in range(len(rainfall)):
-        subcatchment.append(g.update(2*i,0.5,rainfall[i]))
-        subcatchment.append(g.update(2*i+1,0.5,rainfall[i]))
-    print(f"list of depths at each time:{subcatchment}")
-    # print(f"After 2 step: {g.G.vs['depth']}")
-    ts = []
-    for i in range(2*len(rainfall)):
-        ts.append(i*0.5)
-    g.visualize(ts, subcatchment, "connected")
-
+    # rainfall = [0.0,0.5,1.0,0.75,0.5,0.25,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0]
+    # rainfall = [e * 0.0254 for e in rainfall]
+    # g = SubcatchmentGraph(3,2) # temp, 2 just indicates not None
+    # subcatchment = []
+    # for i in range(len(rainfall)):
+    #     subcatchment.append(g.update(2*i,0.5,rainfall[i]))
+    #     subcatchment.append(g.update(2*i+1,0.5,rainfall[i]))
+    # print(f"list of depths at each time:{subcatchment}")
+    # # print(f"After 2 step: {g.G.vs['depth']}")
+    # ts = []
+    # for i in range(2*len(rainfall)):
+    #     ts.append(i*0.5)
+    # g.visualize(ts, subcatchment, "connected")
+    #
