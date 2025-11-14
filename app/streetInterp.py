17fb75a1d73f5e0e41d605eb4590f5b5e6447652
LoganOReed <me@loganreed.org>
2025-11-14 08:17:17 -0500

    Added Second Case for depthFromAreaStreet

--- app/streetInterp.py
+++ app/streetInterp.py
@@ -13,13 +13,16 @@ def depthFromAreaStreet(A, ps):
     # below crown
     belowCrownArea = 0.5 * ps["T_crown"] * ps["Sx"] * ps["T_crown"]
     # between crown and curb
-    betweenCrownAndCurbArea = belowCrownArea + (ps["T_crown"] * (ps["H_curb"] - ps["H_curb"]))
+    betweenCrownAndCurbArea = belowCrownArea + (ps["T_crown"] * (ps["H_curb"] - ps["Sx"] * ps["T_crown"]))
+    pprint(f"{betweenCrownAndCurbArea}")
     # above curb
     # aboveCurbArea = 
 
-    if A <= 0.5 * ps["T_crown"] * ps["Sx"] * ps["T_crown"]:
+    if A <= belowCrownArea:
         d = np.sqrt(2*ps["Sx"]*A)
-    # elif A > 
+    elif A > belowCrownArea and A <= betweenCrownAndCurbArea:
+        d = (A - belowCrownArea) / ps["T_crown"]
+        d = d + ps["Sx"] * ps["T_crown"]
     return d
 
 if __name__ == "__main__":
@@ -35,29 +38,30 @@ if __name__ == "__main__":
     # pprint(S_x)
     # pprint(np.tan(theta))
 
-    xs = np.linspace(0,ps["Sx"]*ps["T_crown"],50)
+    maxAreaInStreet = 0.5 * ps["T_crown"] * ps["Sx"] * ps["T_crown"] + (ps["T_crown"] * (ps["H_curb"] - ps["Sx"] * ps["T_crown"]))
+    xs = np.linspace(0,maxAreaInStreet,50)
     ys = [depthFromAreaStreet(x,ps) for x in xs]
-    # plt.plot(xs,ys,label="depth from area")
-    # plt.show()
-    pprint(f"max height: {ps["Sx"]*ps["T_crown"]} vs max of function: {depthFromAreaStreet(0.5 * ps["T_crown"] * ps["Sx"] * ps["T_crown"],ps)}")
-
-
-
-    N = 100 
-    # get original street function
-    
-    xs = np.linspace(-ps["T_curb"],ps["T_crown"],N)
-    ys = np.zeros(len(xs))
-    for i in range(len(xs)):
-        if xs[i] < 0:
-            ys[i] = - ps["S_back"] * xs[i] + ps["H_curb"]
-        else:
-            ys[i] = ps["Sx"] * xs[i]
-    
-    cs = sp.interpolate.CubicSpline(xs,ys)
-    
-    plt.plot(xs,ys, label="orig")
-    plt.plot(xs, cs(xs), label="S")
-    plt.plot(xs, cs(xs,1), label="S'")
-    plt.saveas(f"figures/streetCubicSpline.png")
+    plt.plot(xs,ys,label="depth from area")
     plt.show()
+    pprint(f"max height: {ps["Sx"]*ps["T_crown"]} vs max of function: {depthFromAreaStreet(0.5 * ps["T_crown"] * ps["Sx"] * ps["T_crown"],ps)}")
+    pprint(f"max height in street: {ps["H_curb"]} vs max of function: {depthFromAreaStreet(maxAreaInStreet,ps)}")
+
+
+    # N = 100 
+    # # get original street function
+    #
+    # xs = np.linspace(-ps["T_curb"],ps["T_crown"],N)
+    # ys = np.zeros(len(xs))
+    # for i in range(len(xs)):
+    #     if xs[i] < 0:
+    #         ys[i] = - ps["S_back"] * xs[i] + ps["H_curb"]
+    #     else:
+    #         ys[i] = ps["Sx"] * xs[i]
+    #
+    # cs = sp.interpolate.CubicSpline(xs,ys)
+    #
+    # plt.plot(xs,ys, label="orig")
+    # plt.plot(xs, cs(xs), label="S")
+    # plt.plot(xs, cs(xs,1), label="S'")
+    # plt.saveas(f"figures/streetCubicSpline.png")
+    # plt.show()
