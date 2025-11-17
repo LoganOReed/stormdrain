8095ca5439c57bf654b04c3d766ffa0bf973516a
LoganOReed <me@loganreed.org>
2025-11-17 17:21:48 -0500

    Created Cubic Spline Figure

--- app/streetInterp.py
+++ app/streetInterp.py
@@ -42,34 +42,11 @@ if __name__ == "__main__":
         "S_back" : 0.02*ftToM,
         "Sx": 0.02*ftToM
             }
-    # theta = np.arcsin(S_x)
-    # pprint(S_x)
-    # pprint(np.tan(theta))
-
-    # maxAreaInStreet = 0.5 * ps["T_crown"] * ps["Sx"] * ps["T_crown"] + (ps["T_crown"] * (ps["H_curb"] - ps["Sx"] * ps["T_crown"]))
-    # xs = np.linspace(0,maxAreaInStreet,50)
-    # ys = [depthFromAreaStreet(x,ps) for x in xs]
-    # plt.plot(xs,ys,label="depth from area")
-    # plt.show()
-    # pprint(f"max height: {ps["Sx"]*ps["T_crown"]} vs max of function: {depthFromAreaStreet(0.5 * ps["T_crown"] * ps["Sx"] * ps["T_crown"],ps)}")
-    # pprint(f"max height in street: {ps["H_curb"]} vs max of function: {depthFromAreaStreet(maxAreaInStreet,ps)}")
-
-
-    N = 100 
-    # get original street function
-    
-    xs = np.linspace(-ps["T_curb"],ps["T_crown"],N)
-    ys = np.zeros(len(xs))
-    for i in range(len(xs)):
-        if xs[i] < 0:
-            ys[i] = - ps["S_back"] * xs[i] + ps["H_curb"]
-        else:
-            ys[i] = ps["Sx"] * xs[i]
-    
-    cs = sp.interpolate.CubicSpline(xs,ys)
-    
-    plt.plot(xs,ys, label="orig")
-    plt.plot(xs, cs(xs), label="S")
-    plt.plot(xs, cs(xs,1), label="S'")
-    # plt.saveas(f"figures/streetCubicSpline.png")
+    maxAreaInStreet = 0.5 * ps["T_crown"] * ps["Sx"] * ps["T_crown"] + (ps["T_crown"] * (ps["H_curb"] - ps["Sx"] * ps["T_crown"]))
+    xs = np.linspace(0,maxAreaInStreet,50)
+    ys = [depthFromAreaStreet(x,ps) for x in xs]
+    plt.plot(xs,ys,label="depth from area")
     plt.show()
+    pprint(f"max height: {ps["Sx"]*ps["T_crown"]} vs max of function: {depthFromAreaStreet(0.5 * ps["T_crown"] * ps["Sx"] * ps["T_crown"],ps)}")
+    pprint(f"max height in street: {ps["H_curb"]} vs max of function: {depthFromAreaStreet(maxAreaInStreet,ps)}")
+
